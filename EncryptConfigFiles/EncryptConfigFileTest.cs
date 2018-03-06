1ee4986edf54ffaa6969f3cf9d1bb7cb3ca54467
Regis ROY <regis.roy@generativeobjects.com>
2018-03-06 18:21:15 +0100

    first commit

---
+++ EncryptConfigFiles/EncryptConfigFileTest.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Web.Configuration;
+using System.Xml;
+using H3C.Tests.BusinessLayer.Common;
+using NFluent;
+using NUnit.Framework;
+
+namespace EncryptConfigFiles
+{
+	[TestFixture]
+	public class EncryptConfigFileTest
+	{
+
+		[Test]
+		[Ignore("testing ideas")]
+		public void EncryptConfigFileAppsettings()
+		{
+			XmlDocument doc;
+			var confilePath = TestHelper.GetFullFilePathInAssembly("files/web.safe.config");
+
+			var config = GetConfigFromPath(confilePath);
+
+			//ProtectSection("appSettings", "RSAProtectedConfigurationProvider", config);      //WORKING
+			//ProtectSection("safe", "RSAProtectedConfigurationProvider", config);               //WORKING
+			//ProtectSection("appSettings/safe", "RSAProtectedConfigurationProvider", config); //NOT WORKING
+
+			//doc = LoadXmlDocument(confilePath);
+			//doc.CheckNodeInnerXmlStartWith("/configuration/appSettings", "<EncryptedData ");
+//			doc.CheckNodeInnerXmlStartWith("/configuration/system.web/machineKey", "<EncryptedData ");
+//			FreeFile();
+
+		}
+		
+		[Test]
+		public void EncryptConfigFile()
+		{
+			XmlDocument doc;
+			var confilePath = TestHelper.GetFullFilePathInAssembly("files/web.config");
+
+			doc = LoadXmlDocument(confilePath);
+			doc.CheckNodeInnerXmlStartWith("/configuration/connectionStrings", "<add name=");
+			doc.CheckNodeHasAttribute("/configuration/system.web/machineKey", "validationKey");
+			FreeFile();
+
+			//Configuration webConfig = GetWebConfig("C:\inetpub\wwwroot\GenerativeObjectsApplications\Sandbox");
+			//var config = GetConfigFromPath(@"C:\inetpub\wwwroot\GenerativeObjectsApplications\Sandbox\Web.config");
+			var config = GetConfigFromPath(confilePath);
+			//Console.WriteLine(config.FilePath);
+
+			ProtectSection("connectionStrings", "RSAProtectedConfigurationProvider", config);
+			ProtectSection("system.web/machineKey", "RSAProtectedConfigurationProvider", config);
+
+			doc = LoadXmlDocument(confilePath);
+			doc.CheckNodeInnerXmlStartWith("/configuration/connectionStrings", "<EncryptedData ");
+			doc.CheckNodeInnerXmlStartWith("/configuration/system.web/machineKey", "<EncryptedData ");
+			FreeFile();
+
+			UnProtectSection("system.web/machineKey", config);
+			UnProtectSection("connectionStrings", config);
+
+			doc = LoadXmlDocument(confilePath);
+			doc.CheckNodeInnerXmlStartWith("/configuration/connectionStrings", "<add name=");
+			doc.CheckNodeHasAttribute("/configuration/system.web/machineKey", "validationKey");
+		}
+		
+		[Test]
+		public void DecryptConfigFile_fromAnotherServer()
+		{
+			XmlDocument doc;
+			var confilePath = TestHelper.GetFullFilePathInAssembly("files/web.encrypt.config");
+
+			doc = LoadXmlDocument(confilePath);
+			doc.CheckNodeInnerXmlStartWith("/configuration/connectionStrings", "<EncryptedData ");
+			FreeFile();
+
+			var config = GetConfigFromPath(confilePath);
+
+			UnProtectSection("connectionStrings", config);
+
+			doc = LoadXmlDocument(confilePath);
+			doc.CheckNodeInnerXmlStartWith("/configuration/connectionStrings", "<add name=");
+		}
+
+		private static XmlDocument LoadXmlDocument(string confilePath)
+		{
+			XmlDocument doc;
+			doc = new XmlDocument();
+			doc.Load(confilePath);
+			return doc;
+		}
+
+		private static void FreeFile()
+		{
+			XmlDocument doc;
+			doc = null;
+			GC.Collect();
+			GC.WaitForPendingFinalizers();
+		}
+
+		private void ProtectSection(string sectionName, string provider, Configuration config)
+		{
+			ConfigurationSection section = config.GetSection(sectionName);
+
+			//if (section != null && !section.SectionInformation.IsProtected)
+			if (section != null)
+			{
+				section.SectionInformation.ProtectSection(provider);
+				config.Save();
+			}
+		}
+
+		private void UnProtectSection(string sectionName, Configuration config)
+		{
+			ConfigurationSection section = config.GetSection(sectionName);
+
+//			if (section != null && section.SectionInformation.IsProtected)
+			if (section != null)
+			{
+				section.SectionInformation.UnprotectSection();
+				config.Save();
+			}
+		}
+
+		private static Configuration GetConfigFromPath(string configFilePath)
+		{
+			ExeConfigurationFileMap map = new ExeConfigurationFileMap {ExeConfigFilename = configFilePath};
+			Configuration config = ConfigurationManager.OpenMappedExeConfiguration(map, ConfigurationUserLevel.None);
+			return config;
+		}
+
+		public static Configuration GetWebConfig(string wwwPath)
+		{
+			var webConfigFile = new FileInfo("Web.config");
+			var vdm = new VirtualDirectoryMapping(wwwPath, true, webConfigFile.Name);
+			var wcfm = new WebConfigurationFileMap();
+			wcfm.VirtualDirectories.Add("/", vdm);
+			var siteName = "Sandbox";
+			return WebConfigurationManager.OpenMappedWebConfiguration(wcfm, "/", siteName);
+		}
+	}
+
+	public static class Extensions
+	{
+		public static XmlElement GetNode(this XmlDocument doc, string nodePath)
+		{
+			XmlElement node = doc.SelectSingleNode(nodePath) as XmlElement;
+			return node;
+		}
+
+		public static void CheckNodeHasAttribute(this XmlDocument doc, string nodePath, string attribute)
+		{
+			var node = doc.GetNode(nodePath);
+			Check.That(node).IsNotNull();
+			Check.That(node.HasAttribute(attribute)).IsTrue();
+		}
+
+		public static void CheckNodeHasNoAttribute(this XmlDocument doc, string nodePath, string attribute)
+		{
+			var node = doc.GetNode(nodePath);
+			Check.That(node).IsNotNull();
+			Check.That(node.HasAttribute(attribute)).IsFalse();
+		}
+
+		public static void CheckNodeInnerXmlStartWith(this XmlDocument doc, string nodePath, string expected)
+		{
+			var node = doc.GetNode(nodePath);
+			Check.That(node).IsNotNull();
+			Check.That(node.InnerXml).StartsWith(expected);
+		}
+	}
+}
---
+++ EncryptConfigFiles/TestHelper.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Configuration;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Reflection;
+
+namespace H3C.Tests.BusinessLayer.Common
+{
+	public class TestHelper
+	{
+		public static readonly string DATE_FORMAT = "yyyy/MM/dd";
+
+		public static DateTime? GetDate(string yyyyMMdd)
+		{
+			if (yyyyMMdd == null)
+			{
+				return null;
+			}
+
+			return DateTime.ParseExact(yyyyMMdd, DATE_FORMAT, CultureInfo.InvariantCulture);
+		}
+
+		public static string GetFullFilePathInAssembly(string relativeFilePathInAssembly, bool createIfDoesntExist = false)
+		{
+			var dirExe = GetExecutionDir();
+			var fullFilePath = Path.Combine(dirExe, relativeFilePathInAssembly);
+			if (createIfDoesntExist)
+			{
+				var fileInfo = new FileInfo(fullFilePath);
+				if (!fileInfo.Exists)
+				{
+					fileInfo.Create();
+				}
+			}
+
+			return fullFilePath;
+		}
+
+		public static string GetExecutionDir()
+		{
+			var uriExe = new Uri(Assembly.GetExecutingAssembly().GetName().CodeBase);
+			var dirExe = Path.GetDirectoryName(uriExe.LocalPath);
+			Debug.Assert(dirExe != null, "dirExe != null");
+			return dirExe;
+		}
+
+		public static void ChangeConfigurationManagerFile(string relativeFilePathInAssembly)
+		{
+			var fullFilePathInAssembly = GetFullFilePathInAssembly(relativeFilePathInAssembly);
+
+			var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+			var appSettings = (AppSettingsSection) config.GetSection("appSettings");
+			appSettings.Settings.Clear();
+			config.AppSettings.File = fullFilePathInAssembly;
+			config.Save(ConfigurationSaveMode.Modified);
+			ConfigurationManager.RefreshSection("appSettings");
+		}
+
+		public static DirectoryInfo CreateTmpDir()
+		{
+			var executionDir = GetExecutionDir();
+			var tmpDir = new DirectoryInfo(Path.Combine(executionDir, Guid.NewGuid().ToString()));
+			tmpDir.Create();
+
+			return tmpDir;
+		}
+
+		public static string CreateFile(string tmpDir, string fileName, DateTime creationDate = default(DateTime))
+		{
+			var fullFileName = Path.Combine(tmpDir, fileName);
+			{
+				File.Create(fullFileName).Close();
+			}
+			if (creationDate != default(DateTime))
+			{
+				File.SetCreationTime(fullFileName, creationDate);
+			}
+
+			return fullFileName;
+		}
+	}
+}
